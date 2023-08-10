dbe837678b9bcf5d4e71bd7524473c250330da50
awukuvincent <vincentawuku@gmail.com>
2023-08-10 12:44:01 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - this is the main function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	put("\"Programming is like building a multilingual puzzle);
+	return(0);
+}
+
