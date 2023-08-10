67b9c044aaa745ad840d33cba9e654ca0b8dbace
awukuvincent <vincentawuku@gmail.com>
2023-08-10 13:48:10 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+
+/**
+ * main - where the execution of the program begins
+ *
+ * Return: always 0 when successful
+ */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
