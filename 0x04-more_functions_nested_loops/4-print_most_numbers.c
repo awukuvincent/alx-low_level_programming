88462fe34c6778a1beb49776ad7faa28df840e31
awukuvincent <vincentawuku@gmail.com>
2023-08-17 18:34:01 -0700

    most_number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+		{
+		if (c != 2 && c != 4)
+			_putchar(c + '0');
+		}
+	_putchar('\n');
+}
